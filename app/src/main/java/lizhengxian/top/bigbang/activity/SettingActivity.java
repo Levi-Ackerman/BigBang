5cf6c1b02141851f31e9109a1d440efe8a9aa9c3
李争献 <zhengxian.lzx@alibaba-inc.com>
2016-10-23 03:32:25 +0800

    测试开关OK了

--- app/src/main/java/lizhengxian/top/bigbang/activity/SettingActivity.java
+++ app/src/main/java/lizhengxian/top/bigbang/activity/SettingActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import lizhengxian.top.bigbang.BigBangService;
@@ -12,47 +16,62 @@ import lizhengxian.top.bigbang.tool.HTTPRequest;
 import lizhengxian.top.bigbang.tool.IResponse;
 import lizhengxian.top.bigbang.R;
 
-public class SettingActivity extends Activity {
+public class SettingActivity extends Activity implements CompoundButton.OnCheckedChangeListener,View.OnClickListener {
     String TAG = getClass().getName();
 
     ViewGroup mRootView ;
+    Switch mRunSwitch;
+    Button mTestBtn;
+    EditText mTestText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRootView = (ViewGroup) findViewById(R.id.root_view);
-        startService(new Intent(getApplicationContext(), BigBangService.class));
+        mRunSwitch = (Switch) findViewById(R.id.sw_run);
+        mRunSwitch.setOnCheckedChangeListener(this);
+        mRunSwitch.setChecked(true);
+        mTestBtn = (Button)findViewById(R.id.test_ws);
+        mTestBtn.setOnClickListener(this);
+        mTestText = (EditText) findViewById(R.id.test_text);
+    }
 
-        findViewById(R.id.test_ws).setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (b){
+            startService(new Intent(this,BigBangService.class));
+        }else{
+            stopService(new Intent(this,BigBangService.class));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        String text = mTestText.getText().toString();
+        HTTPRequest.getSplitChar(text, new IResponse() {
             @Override
-            public void onClick(View view) {
-
-                HTTPRequest.getSplitChar("测试一下讯飞分词云的效果如何呢?", new IResponse() {
-                    @Override
-                    public void finish(String[] words) {
-                        if (words != null) {
-                            final StringBuilder builder = new StringBuilder("分词结果: ");
-                            for (String word : words) {
-                                builder.append(word).append(" | ");
-                            }
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast.makeText(getApplication(), builder.toString(), Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                        }
+            public void finish(String[] words) {
+                if (words != null) {
+                    final StringBuilder builder = new StringBuilder("分词结果: ");
+                    for (String word : words) {
+                        builder.append(word).append(" | ");
                     }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplication(), builder.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
 
+            @Override
+            public void failure(final String errorMsg) {
+                runOnUiThread(new Runnable() {
                     @Override
-                    public void failure(final String errorMsg) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_SHORT).show();
                     }
                 });
             }
